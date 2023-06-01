222505b9d23c881a4c8b0297b73b7b115ce36587
m4xim1nus <mlv@spirtech.com>
2023-06-01 22:39:30 +0200

    test in main with data

--- main.py
+++ main.py
@@ -24,6 +24,21 @@ if __name__ == "__main__":
         # use safe_load instead load
         config = yaml.safe_load(f)
     datagouv = DataGouvSearcher(config)
-    out = datagouv.get_datasets_by_title_and_desc(config["search"]["subventions"]["title_filter"],config["search"]["subventions"]["description_filter"])
-    print(out.index.size)
+    topdown_out = datagouv.get_datasets_by_title_and_desc(config["search"]["subventions"]["title_filter"],config["search"]["subventions"]["description_filter"])
+    
+#     #print topdown_out basic info
+#     print(f"Nombre de datasets correspondant au filtre de titre ou de description : {topdown_out.id.nunique()}")
+#     print(f"Nombre de fichiers : {topdown_out.shape[0]}")
+#     print(f"Nombre de fichiers uniques : {topdown_out.url.nunique()}")
+#     print(f"Nombre de fichiers par format : {topdown_out.groupby('format').size().to_dict()}")
+#     print(f"Nombre de fichiers par fréquence : {topdown_out.groupby('frequency').size().to_dict()}")
+#     INFO:datagouv_searcher:Nombre de datasets correspondant au filtre de description : 201
+#     INFO:datagouv_searcher:Nombre de datasets correspondant au filtre de titre : 154
+#     Nombre de datasets correspondant au filtre de titre ou de description : 232
+#     Nombre de fichiers : 504
+#     Nombre de fichiers uniques : 468
+#     Nombre de fichiers par format : {'arcgis geoservices rest api': 2, 'csv': 206, 'do?id=jorftext000036040528': 3, 'excel': 1, 'geojson': 7, 'html': 1, 'json': 126, 'odata': 1, 'ods': 2, 'page web': 9, 'pdf': 32, 'shp': 11, 'web page': 13, 'xlb': 2, 'xls': 46, 'xlsx': 31, 'zip': 1}
+#     Nombre de fichiers par fréquence : {'annual': 93, 'irregular': 4, 'monthly': 7, 'punctual': 8, 'quarterly': 21, 'semiannual': 2, 'unknown': 296, 'weekly': 2}
+
+
     datagouv.get_datasets_by_content(config["search"]["subventions"]["column_filter"],config["search"]["subventions"]["content_filter"],file_title_filter="asso")
