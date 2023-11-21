4376e52365a2499b3742d19f39a4e553e9151904
MarcAd <adrai.marc@gmail.com>
2023-11-21 07:12:42 +0100

    chunking + integration in the pipeline

--- scripts/utils/psql_connector.py
+++ scripts/utils/psql_connector.py
@@ -1,3 +1,4 @@
+import logging
 import pandas as pd
 import psycopg2
 from io import StringIO
@@ -11,25 +12,30 @@ class PSQLConnector:
         self.port = port
 
     def connect(self):
+        logger = logging.getLogger(__name__)
         try:
             self.connection = psycopg2.connect(dbname=self.dbname, user=self.user, password=self.password, host=self.host,port=self.port)
             self.cursor = self.connection.cursor()
-            print("Connexion à la base de données réussie")
+            logger.info("Connexion à la base de données réussie")
         except Exception as e:
-            print(f"Erreur lors de la connexion à la base de données: {e}")
+            logger.info(f"Erreur lors de la connexion à la base de données: {e}")
 
     def save_communities_to_sql(self,df):
+        logger = logging.getLogger(__name__)
         # Votre DataFrame Pandas
         # dataframe = pd.read_csv('votre_fichier.csv')  # Si vous chargez depuis un fichier CSV
         # Assurez-vous que les colonnes du DataFrame correspondent à la table
 
+        # Just in Case
+        df["trancheEffectifsUniteLegale"] = df["trancheEffectifsUniteLegale"].astype('Int64')
+        
         try:
             truncate_query = "TRUNCATE TABLE public.communities"
             self.cursor.execute(truncate_query)
             self.connection.commit()
-            print("Table vidée avec succès.")
+            logger.info("Table vidée avec succès.")
         except (Exception, psycopg2.DatabaseError) as error:
-            print("Erreur lors du vidage de la table :", error)
+            logger.info("Erreur lors du vidage de la table :", error)
             self.connection.rollback()
         # Sauvegarder le DataFrame dans un buffer en mémoire sous forme CSV
         output = StringIO()
@@ -49,64 +55,67 @@ class PSQLConnector:
             self.cursor.copy_expert(copy_query, output)
             self.connection.commit()
         except (Exception, psycopg2.DatabaseError) as error:
-            print("Error: %s" % error)
+            logger.info("Error: %s" % error)
             self.connection.rollback()
             self.cursor.close()
             return 1
 
-        print("Copy successful")
+        logger.info("Copy successful")
 
     
-    def save_normalized_data_to_sql(self,df):
-            # Votre DataFrame Pandas
-            # dataframe = pd.read_csv('votre_fichier.csv')  # Si vous chargez depuis un fichier CSV
-            # Assurez-vous que les colonnes du DataFrame correspondent à la table
+    def save_normalized_data_to_sql(self,df,chunk_size=1000):
+        # Votre DataFrame Pandas
+        # dataframe = pd.read_csv('votre_fichier.csv')  # Si vous chargez depuis un fichier CSV
+        # Assurez-vous que les colonnes du DataFrame correspondent à la table
+        logger = logging.getLogger(__name__)
 
-            try:
-                truncate_query = "TRUNCATE TABLE public.normalized_data"
-                self.cursor.execute(truncate_query)
-                self.connection.commit()
-                print("Table vidée avec succès.")
-            except (Exception, psycopg2.DatabaseError) as error:
-                print("Erreur lors du vidage de la table :", error)
-                self.connection.rollback()
-            # Sauvegarder le DataFrame dans un buffer en mémoire sous forme CSV
+        # Reformating (to discuss if we keep it here)
+        
+        df['record_id'] = df.index
+        df["export_date"] = pd.Timestamp.now().strftime('%Y-%m-%d')
+        col_order = ['record_id', 'export_date'] + [col for col in df.columns if col not in ['record_id', 'export_date']]
+        df = df[col_order]
+        df = df.drop(columns=["nom","type","source"])
+
+        try:
+            truncate_query = "TRUNCATE TABLE public.normalized_data"
+            self.cursor.execute(truncate_query)
+            self.connection.commit()
+            logger.info("Table vidée avec succès.")
+        except (Exception, psycopg2.DatabaseError) as error:
+            logger.info("Erreur lors du vidage de la table :", error)
+            self.connection.rollback()
+        # Sauvegarder le DataFrame dans un buffer en mémoire sous forme CSV
+        
+        for k in range(0,df.index.size,chunk_size):
             output = StringIO()
-            df.to_csv(output, sep='\t', header=False, index=False)
+            df[k:k+chunk_size].to_csv(output, sep='\t', header=False, index=False)
             output.seek(0)
 
             # Préparer la requête COPY
-            
+
 
             # Copier les données du buffer dans la table
             try:
                 self.cursor.copy_expert("COPY public.normalized_data FROM STDIN WITH CSV DELIMITER '\t' NULL ''", output)
                 self.connection.commit()
             except (Exception, psycopg2.DatabaseError) as error:
-                print("Error: %s" % error)
+                logger.info("Error: %s" % error)
                 self.connection.rollback()
-                self.cursor.close()
                 return 1
 
             print("Copy successful")
 
-
     def close_connection(self):
+        logger = logging.getLogger(__name__)
         self.cursor.close()
         self.connection.close()
-        print("Connexion à la base de données fermée")
+        logger.info("Connexion à la base de données fermée")
 
 # Exemple d'utilisation
 # connector = PSQLConnector('postgres', 'postgres.gaaokuyqioaobxxyapbt', '7kFFZ8xNa9T8Bqm', 'aws-0-eu-central-1.pooler.supabase.com',6543)
 # connector.connect()
-# import pandas as pd
-# sel_com_df = pd.read_excel('./data/communities/processed_data/selected_communities_data_excel.xlsx')
-# sel_com_df["trancheEffectifsUniteLegale"] = sel_com_df["trancheEffectifsUniteLegale"].astype('Int64')
+
 # connector.save_communities_to_sql(sel_com_df)
-# normalized_df = pd.read_csv('./data/datasets/subventions/outputs/normalized_data.csv',sep=";")
-# normalized_df['record_id'] = normalized_df.index
-# normalized_df["export_date"] = pd.Timestamp.now().strftime('%Y-%m-%d')
-# col_order = ['record_id', 'export_date'] + [col for col in normalized_df.columns if col not in ['record_id', 'export_date']]
-# normalized_df = normalized_df[col_order]
-# normalized_df = normalized_df.drop(columns=["nom","type","source"])
+
 # connector.save_normalized_data_to_sql(normalized_df)
