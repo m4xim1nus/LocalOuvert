fa5881b8e72e17a3f00affb1274da14559c6e14e
m4xim1nus <mlv@spirtech.com>
2023-11-23 21:28:51 +0100

    Cleaned data and added unicodecsv library

--- scripts/datasets/datafile_loader.py
+++ scripts/datasets/datafile_loader.py
@@ -1,7 +1,9 @@
 import json
+import re
 import requests
 import pandas as pd
 import logging
+import unidecode
 
 from communities_selector import CommunitiesSelector
 from files_operation import load_from_url, load_json
@@ -13,7 +15,9 @@ class DatafileLoader():
 
         self.scope = CommunitiesSelector(config["communities"])
         self.schema = self.load_schema(config)
-        self.normalized_data = self.load_data(config)
+        self.loaded_data = self.load_data(config)
+        self.normalized_data = self.clean_data(config)
+        # self.normalized_data = self.normalize_data(config)
 
     def load_schema(self, config):
         json_schema = load_from_url(config["search"]["marches_publics"]["schema"]["url"])
@@ -27,3 +31,58 @@ class DatafileLoader():
         df = flatten_data(data['marches'])
         self.logger.info(f"Le fichier au format JSON a été téléchargé avec succès à l'URL : {config['search']['marches_publics']['unified_dataset']['url']}")
         return df
+    
+    def clean_data(self, config):
+        # Clean columns : remove columns from loaded_data whose names are not in schema
+        cleaned_column_names_for_comparison = {
+            self.clean_column_name_for_comparison(col): col for col in self.loaded_data.columns
+        }
+        # Récupérer les noms de propriétés du schéma
+        schema_columns = set(self.schema['property'])
+        # Trouver les colonnes à conserver en fonction du schéma
+        columns_to_keep = set()
+        for cleaned_name, original_name in cleaned_column_names_for_comparison.items():
+            if cleaned_name in schema_columns:
+                columns_to_keep.add(original_name)
+        # Conserver uniquement les colonnes qui correspondent au schéma
+        cleaned_data = self.loaded_data.filter(columns_to_keep)
+
+        self.logger.info(f"Nettoyage des colonnes terminé, {len(columns_to_keep)} colonnes conservées.")
+
+        # To do better: specifif marchés publics rows cleaning (mixed with concessions data in source file)
+        procedure_values = self.get_schema_values('procedure', 'enum')
+        nature_values = self.get_schema_values('nature', 'enum')
+        type_pattern = self.get_schema_value('_type', 'pattern')
+        
+        cleaned_data = cleaned_data[
+            cleaned_data['procedure'].apply(self.matches_values, args=(procedure_values,)) |
+            cleaned_data['nature'].apply(self.matches_values, args=(nature_values,)) |
+            cleaned_data['_type'].str.match(type_pattern)
+        ]
+
+        return cleaned_data
+
+    def clean_column_name_for_comparison(self, column_name):
+        # Supprimer les indices numériques et les points supplémentaires du nom de colonne
+        return re.sub(r'\.\d+\.', '.', column_name)
+    
+    def get_schema_values(self, property_name, column_name):
+        # Récupérer les valeurs ou le pattern du schéma pour une propriété donnée
+        values = self.schema.loc[self.schema['property'] == property_name, column_name].iloc[0]
+        return [self.clean_value(value) for value in values] if isinstance(values, list) else self.clean_value(values)
+
+    def get_schema_value(self, property_name, column_name):
+        # Récupérer une valeur unique du schéma pour une propriété donnée
+        return self.schema.loc[self.schema['property'] == property_name, column_name].iloc[0]
+
+    def clean_value(self, value):
+        # Nettoyer une valeur en retirant les accents, en mettant en minuscule et en supprimant certains caractères
+        value = unidecode.unidecode(value).lower()
+        return re.sub(r"[,']", "", value)
+
+    def matches_values(self, value, values):
+        # Vérifier si la valeur nettoyée correspond à l'une des valeurs nettoyées du schéma
+        if pd.isna(value):
+            return False
+        cleaned_value = self.clean_value(value)
+        return cleaned_value in values
