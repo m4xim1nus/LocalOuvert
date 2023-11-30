e9c9456483a12778e327b32ba822fa754cbe5d6a
m4xim1nus <mlv@spirtech.com>
2023-12-01 00:16:11 +0100

    Correction...

--- main.py
+++ main.py
@@ -46,19 +46,19 @@ if __name__ == "__main__":
     files_in_scope_filename = "files_in_scope.csv"
     save_csv(subventions_files_in_scope, subventions_data_folder, files_in_scope_filename, sep=";")
 
-    # # Build new object taking files_in_scope & config as inputs in init, to load the subventions_datafiles, normalize them and save them in a new folder.
-    # subventions_datafiles = DatafilesLoader(subventions_files_in_scope,config)
-    # # Save the normalized data in a csv file
-    # normalized_data_filename = "normalized_data.csv"
-    # save_csv(subventions_datafiles.normalized_data, subventions_data_folder, normalized_data_filename, sep=";")
-    # # Save the list of files that are not readable in a csv file
-    # datafiles_out_filename = "datafiles_out.csv"
-    # save_csv(subventions_datafiles.datafiles_out, subventions_data_folder, datafiles_out_filename, sep=";")
-    # # Save the list of files that have columns not in common with the schema in a csv file
-    # datacolumns_out_filename = "datacolumns_out.csv"
-    # save_csv(subventions_datafiles.datacolumns_out, subventions_data_folder, datacolumns_out_filename, sep=";")
+    # Build new object taking files_in_scope & config as inputs in init, to load the subventions_datafiles, normalize them and save them in a new folder.
+    subventions_datafiles = DatafilesLoader(subventions_files_in_scope,config)
+    # Save the normalized data in a csv file
+    normalized_data_filename = "normalized_data.csv"
+    save_csv(subventions_datafiles.normalized_data, subventions_data_folder, normalized_data_filename, sep=";")
+    # Save the list of files that are not readable in a csv file
+    datafiles_out_filename = "datafiles_out.csv"
+    save_csv(subventions_datafiles.datafiles_out, subventions_data_folder, datafiles_out_filename, sep=";")
+    # Save the list of files that have columns not in common with the schema in a csv file
+    datacolumns_out_filename = "datacolumns_out.csv"
+    save_csv(subventions_datafiles.datacolumns_out, subventions_data_folder, datacolumns_out_filename, sep=";")
 
     
-    # marches_publics_data_folder = Path(get_project_base_path()) / "data" / "datasets" / "marches_publics" / "outputs"
-    # marches_publics = DatafileLoader(config)
-    # save_csv(marches_publics.normalized_data, marches_publics_data_folder, normalized_data_filename, sep=";")
+    marches_publics_data_folder = Path(get_project_base_path()) / "data" / "datasets" / "marches_publics" / "outputs"
+    marches_publics = DatafileLoader(config)
+    save_csv(marches_publics.normalized_data, marches_publics_data_folder, normalized_data_filename, sep=";")
