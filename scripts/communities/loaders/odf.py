fa64b4b2da2edc159cb12ed82ad6867e903a065c
MAD <mad@MADs-MacBook-Air.local>
2024-01-29 16:26:22 +0100

    small refacto

--- scripts/communities/loaders/odf.py
+++ scripts/communities/loaders/odf.py
@@ -1,37 +1,39 @@
-from pathlib import Path
-import pandas as pd
-from typing import Optional, Dict, Any
-
-from files_operation import save_csv
-from scripts.loaders.base_loader import BaseLoader
-from config import get_project_base_path
-
 from scripts.utils.data_schema import OdfSchema
 from scripts.utils.data_columns import OdfDF
+from scripts.utils.loader import BaseLoader
+from scripts.utils.file_utils import save_csv
+from typing import Dict, Any
+from pathlib import Path
+import pandas as pd
+from utils import get_project_base_path
 
 class OdfLoader:
     def __init__(self, config: Dict[str, Any]) -> None:
-        data_folder: Path = Path(get_project_base_path()) / config["processed_data"]["path"]
-        data_file: Path = data_folder / config["processed_data"]["filename"]
+        self.config = config
+        self.load_data()
+    
+    def load_data(self) -> None:
+        data_folder = Path(get_project_base_path()) / self.config["processed_data"]["path"]
+        data_file = data_folder / self.config["processed_data"]["filename"]
         
         if data_file.exists():
-            odf_data: pd.DataFrame = pd.read_csv(data_file, sep=";")
+            self.data = pd.read_csv(data_file, sep=";")
         else:
-            odf_data_loader: BaseLoader = BaseLoader.loader_factory(config["url"], dtype=config["dtype"])
-            odf_data: pd.DataFrame = odf_data_loader.load()
+            odf_data_loader = BaseLoader.loader_factory(self.config["url"], dtype=self.config["dtype"])
+            self.data = odf_data_loader.load()
         
-        # Making sure we trim everything in the SIREN column
-        odf_data[OdfDF.siren] = odf_data[OdfDF.siren].str.replace('[^0-9]', '', regex=True)
+        # Making sure we trim everything in the SIREN column so it can be an Int
+        self.data[OdfDF.siren] = self.data[OdfDF.siren].str.replace('[^0-9]', '', regex=True)
         
-        validated_odf_data: pd.DataFrame = OdfSchema.validate(odf_data)
+        validated_odf_data = OdfSchema.validate(self.data)
         self.data = validated_odf_data
         
         if not data_file.exists():
-            # Save the processed data to a CSV file
-            self.save(Path(config["processed_data"]["path"]), config["processed_data"]["filename"])
+            self.save_data()
     
     def get(self) -> pd.DataFrame:
         return self.data
     
-    def save(self, path: Path, filename: str, sep: str = ";", index: bool = True) -> None:
-        save_csv(self.data, path, filename, sep=sep, index=index)
+    def save_data(self) -> None:
+        data_folder = Path(get_project_base_path()) / self.config["processed_data"]["path"]
+        save_csv(self.data, data_folder, self.config["processed_data"]["filename"], sep=";", index=True)
